b30cf920c9e79a4b4a8a096aa6315ed7ff93a15a
tverum <tim.vanerum@ugent.be>
2020-04-05 03:25:26 +0200

    Add requirements

--- donza/donza/settings.py
+++ donza/donza/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'management.apps.ManagementConfig',
+    'phonenumber_field',
+    'localflavor',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
