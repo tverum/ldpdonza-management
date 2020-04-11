40550e45b7542448963ed6c0d49b409fac111c6b
tverum <tim.vanerum@ugent.be>
2020-04-11 12:06:08 +0200

    Voeg Ouder, Seizoen, Ploeglid relaties toe

--- donza/management/models.py
+++ donza/management/models.py
@@ -25,6 +25,37 @@ class Ouder(models.Model):
         return self.voornaam + " " + self.familienaam
 
 
+class Seizoen(models.Model):
+
+    seizoen_id = models.AutoField(primary_key=True)
+    naam = models.CharField(max_length=20)
+    startdatum = models.DateField()
+    einddatum = models.DateField()
+    bezig = models.BooleanField()
+
+    def __str__(self):
+        return "{} ({}-{})".format(self.naam, self.startdatum.year, self.einddatum.year)
+
+
+class Ploeg(models.Model):
+
+    ploeg_id = models.AutoField(primary_key=True)
+    seizoen = models.ForeignKey(
+        'management.Seizoen', on_delete=models.DO_NOTHING)
+    naam = models.CharField(max_length=20)
+    korte_naam = models.CharField(max_length=5)
+    leeftijdscategorie = models.IntegerField()
+
+    def __str__(self):
+        return "{} ({}-{})".format(self.naam, self.seizoen.startdatum.year, self.seizoen.einddatum.year)
+
+class PloegLid(models.Model):
+
+    ploeg_id = models.ForeignKey('management.Ploeg', on_delete=models.DO_NOTHING)
+    lid_id = models.ForeignKey('management.Lid', on_delete=models.DO_NOTHING)
+    functie = models.ForeignKey('management.Functie', on_delete=models.DO_NOTHING)
+
+
 class Lid(models.Model):
 
     MAN = "m"
@@ -36,56 +67,6 @@ class Lid(models.Model):
         (ANDER, "Verkies niet te zeggen"),
     ]
 
-    HEADER_NAMEN = [
-        "#",
-        "Voornaam",
-        "Familienaam",
-        "Geslacht",
-        "Betalend?",
-        "Functies",
-        "Straatnaam",
-        "Huisnr.",
-        "Bus",
-        "Postcode",
-        "Gemeente",
-        "Geboortedat.",
-        "Gsm",
-        "Moeder",
-        "Vader",
-        "Email",
-        "Gescheiden?",
-        "Extra",
-        "Rekeningnr.",
-        "VBL",
-        "Updated",
-        "Created",
-    ]
-
-    FIELD_NAMEN = [
-        "club_id",
-        "voornaam",
-        "familienaam",
-        "geslacht",
-        "betalend_lid",
-        "functies",
-        "straatnaam",
-        "huisnummer",
-        "bus",
-        "postcode",
-        "gemeente",
-        "geboortedatum",
-        "gsmnummer",
-        "moeder_id",
-        "vader_id",
-        "email",
-        "gescheiden_ouders",
-        "extra_informatie",
-        "rekeningnummer",
-        "lidnummer_vbl",
-        "updated_at",
-        "created_at",
-    ]
-
     club_id = models.AutoField(primary_key=True)
     voornaam = models.CharField(max_length=20)
     familienaam = models.CharField(max_length=50)
@@ -95,7 +76,6 @@ class Lid(models.Model):
         default=MAN
     )
     betalend_lid = models.BooleanField(default=False)
-    functies = models.ManyToManyField('management.Functie')
     straatnaam = models.CharField(max_length=50)
     huisnummer = models.IntegerField()
     bus = models.CharField(max_length=50, blank=True)
@@ -110,8 +90,6 @@ class Lid(models.Model):
     email = models.EmailField(max_length=254, null=True)
     gescheiden_ouders = models.BooleanField(default=False)
     extra_informatie = models.CharField(default="", max_length=500, blank=True)
-    # TODO: determine if this is really required
-    # rijksregisternummer = models.CharField(max_length=20)
     rekeningnummer = IBANField(null=True)
     lidnummer_vbl = models.IntegerField(null=True, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
