d9c7b5721a7b808617f0c6c5167a3eeeecdea57f
tverum <tim.vanerum@ugent.be>
2020-04-29 18:16:16 +0200

    Change InMemoryChannel by RedisChannel

--- donza/donza/asgi.py
+++ donza/donza/asgi.py
@@ -13,4 +13,17 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'donza.settings')
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+# importeer de websocket urls die gebruikt worden door reactor
+from reactor.urls import websocket_urlpatterns
+
+import django.urls
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(URLRouter(
+        # declare de websocket url patterns gebruikt door Django Reactor
+        websocket_urlpatterns,
+    ))
+})
--- donza/donza/settings/dev.py
+++ donza/donza/settings/dev.py
@@ -1,3 +1,25 @@
 from donza.settings.base import *
 
 DEBUG = True
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vanerum.tim@gmail.com'
+EMAIL_HOST_PASSWORD = 'AA135Tim'
+
+# only for development purposes
+# CHANNEL_LAYERS = {
+#     "default": {
+#        "BACKEND": "channels.layers.InMemoryChannelLayer"
+#    }
+#}
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
