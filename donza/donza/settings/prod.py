46b8e2a33290a7de02eec939b64fcd0c3b28b8a0
tverum <tim.vanerum@ugent.be>
2020-05-13 20:28:49 +0200

    Add SSL-specs

--- donza/donza/settings/prod.py
+++ donza/donza/settings/prod.py
@@ -4,3 +4,8 @@ ALLOWED_HOSTS = ["secretariaat.ldpdonza.be"]
 DEBUG = False
 STATIC_ROOT = '/home/tim/static'
 MEDIA_ROOT = '/home/tim/media'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
