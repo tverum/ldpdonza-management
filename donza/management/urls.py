18399a330e98f30243574c293ada74559e5ef9f0
tverum <tim.vanerum@ugent.be>
2020-05-16 19:09:43 +0200

    Remove obsolete urls

--- donza/management/urls.py
+++ donza/management/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("leden/<int:pk>/", views.LidEditView.as_view(), name="lid"),
     path("leden/<int:pk>/verwijder", views.verwijder_lid, name="verwijder_lid"),
     path("leden/verwerk/", views.verwerk_leden, name="verwerk_leden"),
-    path("leden/verwijder/", views.verwijder_leden, name="verwijder_leden"),
     path("leden/nieuw/", views.LidNewView.as_view(), name='nieuw_lid'),
     path("ouder/new/", views.create_ouder, name="new_ouder"),
     path("ploegen/", views.PloegListView.as_view(), name='ploegen'),
@@ -16,7 +15,6 @@ urlpatterns = [
     path("ploegen/<int:pk>/select/", views.PloegSelectView.as_view(), name='ploeg_select'),
     path("ploegen/<int:pk>/view/", views.PloegView.as_view(), name='ploeg_view'),
     path("betalingen/stuur_mail/<int:pk>/", views.stuur_mail, name="betalingen_mail"),
-    path("betalingen/genereer/", views.genereer, name="betalingen_genereer"),
     path("betalingen/", views.BetalingTableView.as_view(), name="betalingen"),
     path('', views.IndexView.as_view(), name='index'),
 ]
