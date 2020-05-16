ea6f4ae2aab6c28be95327bd0d9be4065bc906ce
tverum <tim.vanerum@ugent.be>
2020-05-17 00:00:56 +0200

    Add generation of coaches accounts

--- donza/management/admin.py
+++ donza/management/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
 
 from .models import Lid, Functie, Ouder, Ploeg, PloegLid, Seizoen, LidgeldKlasse
+
+
+class PloegAdmin(GuardedModelAdmin):
+    pass
+
 
 admin.site.register(Lid)
 admin.site.register(Functie)
 admin.site.register(Ouder)
-admin.site.register(Ploeg)
+admin.site.register(Ploeg, PloegAdmin)
 admin.site.register(PloegLid)
 admin.site.register(Seizoen)
 admin.site.register(LidgeldKlasse)
---
+++ donza/management/management/commands/create_accounts.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from guardian.models import UserObjectPermission
+from django.conf import settings
+
+import os
+import json
+
+from ...models import Lid, Functie, PloegLid, Ploeg
+
+
+class Command(BaseCommand):
+    help = ""
+
+    def handle(self, **options):
+        generate_coaches_accounts()
+
+
+def generate_coaches_accounts():
+    functie_coach = Functie.objects.get(functie="Coach")
+    coaches = Lid.objects.filter(functies__functie=functie_coach)
+
+    entries = []
+    for coach in coaches:
+        username = '{}{}'.format(coach.voornaam.replace(" ", "").lower(), coach.familienaam.replace(" ", "").lower())
+        password = '{}{}'.format(coach.voornaam[::-1].replace(" ", "").lower(),
+                                 coach.familienaam[::-1].replace(" ", "").lower())
+        try:
+            coach_user = User.objects.create_user(
+                username,
+                coach.email,
+                password
+            )
+        except Exception:
+            coach_user = User.objects.get_by_natural_key(username)
+
+        entries.append((coach.voornaam, coach.familienaam, username, coach.email, password,))
+        ploegen = [Ploeg.objects.get(ploeg_id=ploeglid.ploeg.ploeg_id) for ploeglid in PloegLid.objects.filter(
+            lid=coach, functie=functie_coach)]
+        for ploeg in ploegen:
+            UserObjectPermission.objects.assign_perm('view_ploeg', coach_user, obj=ploeg)
+
+    keys = ("Voornaam", "Familienaam", "Username", "Email", "Passwoord",)
+    dictionairy = [dict(zip(keys, entry)) for entry in entries]
+    with open(os.path.join(settings.BASE_DIR, "coaches.json"), 'w') as outfile:
+        json.dump(dictionairy, outfile)
+
+
+if __name__ == '__main__':
+    generate_coaches_accounts()
