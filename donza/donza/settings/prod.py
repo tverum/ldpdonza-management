92dca56e1d034a86312717cd56c512b2929d0c9b
tverum <tim.vanerum@ugent.be>
2020-05-17 11:09:10 +0200

    Fix mailconfig production

--- donza/donza/settings/prod.py
+++ donza/donza/settings/prod.py
@@ -4,3 +4,8 @@ ALLOWED_HOSTS = ["secretariaat.ldpdonza.be"]
 DEBUG = False
 STATIC_ROOT = '/home/tim/static'
 MEDIA_ROOT = '/home/tim/media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp-relay.gmail.com'
+EMAIL_PORT = 587
