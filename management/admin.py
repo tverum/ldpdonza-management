f4621616a9f441e3d226eb2bacc0cc9122206dfc
tverum <tim.vanerum@ugent.be>
2020-07-24 13:13:18 +0200

    Add csv-download to betaling admin panel

--- management/admin.py
+++ management/admin.py
@@ -1,7 +1,34 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 from guardian.admin import GuardedModelAdmin
 
 from .models import Lid, Functie, Ouder, Ploeg, PloegLid, Seizoen, LidgeldKlasse, Betaling
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+@admin.register(Betaling)
+class BetalingAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ("origineel_bedrag", "afgelost_bedrag", "lid", "seizoen", "mails_verstuurd")
+    actions = ["export_as_csv"]
 
 
 class PloegAdmin(GuardedModelAdmin):
@@ -15,4 +42,3 @@ admin.site.register(Ploeg, PloegAdmin)
 admin.site.register(PloegLid)
 admin.site.register(Seizoen)
 admin.site.register(LidgeldKlasse)
-admin.site.register(Betaling)
