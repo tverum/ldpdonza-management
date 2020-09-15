cc45b4d87532590ed4c38510d8c0496fcec48b77
tverum <tim.vanerum@ugent.be>
2020-09-16 01:55:23 +0200

    Fix attempt

--- donza/settings/__init__.py
+++ donza/settings/__init__.py
@@ -2,4 +2,6 @@ from .common import *
 try:
     from .local import *
 except ImportError:
+    import sys
+    sys.path.insert('/home/ubuntu/.local/lib/python3.6/site-packages/')
     from .production import *
