aeb7811e6cb283004a6fe8938525e5813c694507
Tim Van Erum <tim.vanerum@dataroots.io>
2022-12-26 13:45:26 +0100

    Add test fixtures to git tree (2)

---
+++ donza/settings/test.py
@@ -0,0 +1,30 @@
+import os
+from .common import *
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        }
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "ID309280_secretariaat",
+    }
+}
